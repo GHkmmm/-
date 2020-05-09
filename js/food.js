b37c52005cbecc69841a61463169fbd7af5f1ea4
GHkmmm <hgmkk2299@icloud.com>
2020-05-09 16:19:28 +0800

    代码优化

--- js/food.js
+++ js/food.js
@@ -1,38 +1,47 @@
-var position = 'absolute'
-var element = []
+var Food;
+var removeDiv;
 
-function Food(options){
-  options = options || {};
-  this.x = options.x || 0;
-  this.y = options.y || 0; 
-  this.width = options.width || 20;
-  this.height = options.height || 20;
-  this.color = '#fff';
-}
+(function(){
+  var position = 'absolute'
+  var elements = []
 
-Food.prototype.render = function(map){
-  if(element != []){
-    for(var a = element.length-1;a>=0;a--){
-      element[a].parentNode.removeChild(element[a]);
-      element.pop()
+  Food = function(options){
+    options = options || {};
+    this.x = options.x || 0;
+    this.y = options.y || 0; 
+    this.width = options.width || 20;
+    this.height = options.height || 20;
+    this.color = '#fff';
+  }
+
+  Food.prototype.render = function(map){
+    removeDiv(elements);
+    this.randomXY();
+    var div = document.createElement('div');
+    div.style.position = position;
+    div.style.left = this.x * this.width + 'px';
+    div.style.top = this.y * this.height + 'px';
+    div.style.width = this.width + 'px';
+    div.style.height = this.height + 'px';
+    div.style.backgroundColor = this.color;
+    elements.push(div);
+    map.appendChild(div);
+  }
+
+  Food.prototype.randomXY = function(){
+    this.x = Tools.getRandom(0, 35);
+    this.y = Tools.getRandom(0, 25);
+  }
+
+  removeDiv = function(elements){
+    if(elements != []){
+      for(var a = elements.length-1;a>=0;a--){
+        elements[a].parentNode.removeChild(elements[a]);
+        elements.pop()
+      }
     }
   }
-  this.randomXY();
-  var div = document.createElement('div');
-  div.style.position = position;
-  div.style.left = this.x * this.width + 'px';
-  div.style.top = this.y * this.height + 'px';
-  div.style.width = this.width + 'px';
-  div.style.height = this.height + 'px';
-  div.style.backgroundColor = this.color;
-  element.push(div);
-  map.appendChild(div);
-}
-
-Food.prototype.randomXY = function(){
-  this.x = Tools.getRandom(0, 35);
-  this.y = Tools.getRandom(0, 25);
-}
+})()
 
 // var food = new Food();
 // food.randomXY();
--- js/snake.js
+++ js/snake.js
@@ -1,105 +1,105 @@
-var position = "absolute"
-var elements = []
-var that
+var Snake;
 
-function Snake(options){
-  options = options || {}
-  this.snakeItem = [
-    {x: 3, y: 2},
-    {x: 2, y: 2},
-    {x: 1, y: 2}
-  ]
-  this.width = options.width || 20
-  this.height = options.height || 20
-  this.color = options.color || "#fff"
-  this.direction = "right"
-  that = this;
-}
+(function(){
+  var position = "absolute"
+  var elements = []
+  var that
 
-Snake.prototype.render = function(map){
-  //移除上一次的所有元素
-  if(elements != []){
-    for(var a = elements.length-1;a>=0;a--){
-      elements[a].parentNode.removeChild(elements[a]);
-      elements.pop()
+  Snake = function(options){
+    options = options || {}
+    this.snakeItem = [
+      {x: 3, y: 2},
+      {x: 2, y: 2},
+      {x: 1, y: 2}
+    ]
+    this.width = options.width || 20
+    this.height = options.height || 20
+    this.color = options.color || "#fff"
+    this.direction = "right"
+    that = this;
+  }
+
+  Snake.prototype.render = function(map){
+    //移除上一次的所有元素
+    removeDiv(elements);
+    
+    for(var i=0;i<this.snakeItem.length;i++){
+      var div = document.createElement('div');
+      div.style.position = position;
+      div.style.width = this.width + 'px';
+      div.style.height = this.height + 'px';
+      div.style.backgroundColor = this.color;
+      div.style.left = this.snakeItem[i].x * this.width + 'px'
+      div.style.top = this.snakeItem[i].y * this.height + 'px'
+      elements.push(div);
+      map.appendChild(div);
     }
   }
-  
-  for(var i=0;i<this.snakeItem.length;i++){
-    var div = document.createElement('div');
-    div.style.position = position;
-    div.style.width = this.width + 'px';
-    div.style.height = this.height + 'px';
-    div.style.backgroundColor = this.color;
-    div.style.left = this.snakeItem[i].x * this.width + 'px'
-    div.style.top = this.snakeItem[i].y * this.height + 'px'
-    elements.push(div);
-    map.appendChild(div);
-  }
-}
 
-Snake.prototype.move = function(){
-  
-  switch(this.direction){
-    case "right":
-      for(var i = this.snakeItem.length-1;i>=0;i--){
-        if(i == 0){
-          this.snakeItem[0].x++;
-        }else{
-          this.snakeItem[i].x = this.snakeItem[i-1].x;
-          this.snakeItem[i].y = this.snakeItem[i-1].y;
+  Snake.prototype.move = function(){
+    
+    switch(this.direction){
+      case "right":
+        for(var i = this.snakeItem.length-1;i>=0;i--){
+          if(i == 0){
+            this.snakeItem[0].x++;
+          }else{
+            this.snakeItem[i].x = this.snakeItem[i-1].x;
+            this.snakeItem[i].y = this.snakeItem[i-1].y;
+          }
         }
-      }
-      break;
-    case "left":
-      for(var i = this.snakeItem.length-1;i>=0;i--){
-        if(i == 0){
-          this.snakeItem[0].x--;
-        }else{
-          this.snakeItem[i].x = this.snakeItem[i-1].x;
-          this.snakeItem[i].y = this.snakeItem[i-1].y;
+        break;
+      case "left":
+        for(var i = this.snakeItem.length-1;i>=0;i--){
+          if(i == 0){
+            this.snakeItem[0].x--;
+          }else{
+            this.snakeItem[i].x = this.snakeItem[i-1].x;
+            this.snakeItem[i].y = this.snakeItem[i-1].y;
+          }
         }
-      }
-      break;
-    case "top":
-      for(var i = this.snakeItem.length-1;i>=0;i--){
-        if(i == 0){
-          this.snakeItem[0].y--;
-        }else{
-          this.snakeItem[i].x = this.snakeItem[i-1].x;
-          this.snakeItem[i].y = this.snakeItem[i-1].y;
+        break;
+      case "top":
+        for(var i = this.snakeItem.length-1;i>=0;i--){
+          if(i == 0){
+            this.snakeItem[0].y--;
+          }else{
+            this.snakeItem[i].x = this.snakeItem[i-1].x;
+            this.snakeItem[i].y = this.snakeItem[i-1].y;
+          }
         }
-      }
-      break;
-    case "bottom":
-      for(var i = this.snakeItem.length-1;i>=0;i--){
-        if(i == 0){
-          this.snakeItem[0].y++;
-        }else{
-          this.snakeItem[i].x = this.snakeItem[i-1].x;
-          this.snakeItem[i].y = this.snakeItem[i-1].y;
+        break;
+      case "bottom":
+        for(var i = this.snakeItem.length-1;i>=0;i--){
+          if(i == 0){
+            this.snakeItem[0].y++;
+          }else{
+            this.snakeItem[i].x = this.snakeItem[i-1].x;
+            this.snakeItem[i].y = this.snakeItem[i-1].y;
+          }
         }
-      }
-      break;
+        break;
+    }
   }
-}
 
-document.onkeydown = function(e){
-  switch(e.keyCode){
-    case 37:
-      that.direction = "left"
-      break;
-    case 38:
-      that.direction = "top"
-      break;
-    case 39:
-      that.direction = "right"
-      break;
-    case 40:
-      that.direction = "bottom"
-      break;
+  document.onkeydown = function(e){
+    switch(e.keyCode){
+      case 37:
+        that.direction = "left"
+        break;
+      case 38:
+        that.direction = "top"
+        break;
+      case 39:
+        that.direction = "right"
+        break;
+      case 40:
+        that.direction = "bottom"
+        break;
+    }
   }
-}
+
+})()
 
 
 //  测试代码
